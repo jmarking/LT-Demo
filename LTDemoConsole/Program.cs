1d0a32065b705fec1b14584080043b3930c53bbf
Jon Marking <jon.marking@hotmail.com>
2019-04-15 09:11:07 -0500

    - Added MS basic DI for comparison.

--- LTDemoConsole/Program.cs
+++ LTDemoConsole/Program.cs
@@ -2,19 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LTDemoData;
 using LTDemoService.Services;
+using Microsoft.Extensions.DependencyInjection;
 using StructureMap;
 
 namespace LTDemoConsole
 {
     class Program
     {
-        private static readonly Container _container = ConfigureIoC();
+        private static readonly Container _smContainer = ConfigureSM();
+        private static readonly ServiceProvider _msContainer = ConfigureMS();
 
         static async Task Main(string[] args)
         {
-            var albumService = _container.GetInstance<IAlbumService>();
-            var photoService = _container.GetInstance<IPhotoService>();
+            // StructureMap DI
+            //var albumService = _smContainer.GetInstance<IAlbumService>();
+            //var photoService = _smContainer.GetInstance<IPhotoService>();
+
+            // MS DI built into asp.net core
+            var albumService = _msContainer.GetService<IAlbumService>();
+            var photoService = _msContainer.GetService<IPhotoService>();
+
 
             var albumId = await AlbumSelection(albumService);
 
@@ -25,7 +34,10 @@ namespace LTDemoConsole
             Console.ReadKey();
         }
 
-        private static Container ConfigureIoC() =>
+        /// <summary>
+        /// StructureMap
+        /// </summary>
+        private static Container ConfigureSM() =>
             new Container(_ =>
             {
                 _.Scan(x =>
@@ -34,6 +46,17 @@ namespace LTDemoConsole
                     x.WithDefaultConventions();
                 });
             });
+
+        /// <summary>
+        /// Microsoft
+        /// Can use 3rd party packages like Scrutor to achieve scan registration by convention.
+        /// </summary>
+        private static ServiceProvider ConfigureMS() =>
+            new ServiceCollection()
+                .AddSingleton<IAlbumService, AlbumService>()
+                .AddSingleton<IPhotoService, PhotoService>()
+                .AddSingleton<IWebServiceData, WebServiceData>()
+                .BuildServiceProvider();
 
         private async static Task<int> AlbumSelection(IAlbumService albumService)
         {
